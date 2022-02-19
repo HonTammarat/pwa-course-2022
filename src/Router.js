9dc5c4e2fd506460549a0f2909d69f45698c695f
HonTammarat <tammarat.ampo@bumail.net>
2022-02-19 16:57:59 +0700

    Change logo website

--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,7 @@
 import App from './App';
 import Profile from './Profile';
 import MiniGame from './MiniGame';
+import Support from './Support';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 //index.js --> Router.js --> App.js
@@ -9,9 +10,10 @@ function Router(){
     return(
         <BrowserRouter>
             <Routes>
-                <Route path= "/" element ={<App/>} />
-                <Route path= "/profile" element ={<Profile/>} />
-                <Route path= "/minigame" element ={<MiniGame/>} />
+                <Route path = "/" element = {<App/>} />
+                <Route path = "/profile" element = {<Profile />} />
+                <Route path = "/minigame" element = {<MiniGame />} />
+                <Route path = "/support" element = {<Support />} />
             </Routes>
         </BrowserRouter>
     )
---
+++ src/Support.js
@@ -0,0 +1,11 @@
+import './css/Support.css'
+
+function Support(){
+    return(
+        <div className='background-page'>
+            <div></div>
+        </div>
+    )
+}
+
+export default Support.js
