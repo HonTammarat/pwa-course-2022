79b5c4ca9d5b649f76888cd2712844fa3b61292d
HonTammarat <tammarat.ampo@bumail.net>
2022-02-19 17:35:24 +0700

    06 ex01 update

---
+++ src/Login.js
@@ -0,0 +1,15 @@
+import './css/Login.css'
+
+function Login(){
+    return(
+        <div className ='login-page'>
+            <div className='logo'></div>
+            <div className='login'>Username</div>
+            <div className='login'>Password</div>
+            <div className='login'>Login</div>
+            <div className='login'>Forgot Password</div>
+        </div>
+    )
+}
+
+export default Login;
--- src/Router.js
+++ src/Router.js
@@ -1,7 +1,7 @@
 import App from './App';
 import Profile from './Profile';
 import MiniGame from './MiniGame';
-import Support from './Support';
+import Login from './Login';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 //index.js --> Router.js --> App.js
@@ -13,7 +13,7 @@ function Router(){
                 <Route path = "/" element = {<App/>} />
                 <Route path = "/profile" element = {<Profile />} />
                 <Route path = "/minigame" element = {<MiniGame />} />
-                <Route path = "/support" element = {<Support />} />
+                <Route path = "/login" element = {<Login />} />
             </Routes>
         </BrowserRouter>
     )
