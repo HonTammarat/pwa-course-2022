5bcdda470638871678627f9eb5f7be64719bb2bb
HonTammarat <tammarat.ampo@bumail.net>
2022-02-26 16:32:29 +0700

    ส่งงานที่หาวิธีนานแสนนาน

--- src/FriendList.js
+++ src/FriendList.js
@@ -6,15 +6,25 @@ import Avatar from './components/Avatar';
 function FriendList(){
   const [friends, setFriends] = useState([
     {user: "user1", displayName: "ไอ้ปื้ด"},
-    {user: "user2", displayName: "นางสาวส้ม"},
-    {user: "user3", displayName: "พลทหารแดง"},
+    {user: "user2", displayName: "นางสาวสุดา"},
+    {user: "user3", displayName: "พลทหารหนึ่ง"},
     {user: "user4", displayName: "ศาตราจารย์นายกรัฐมนตรีดอกเตอร์เขียว"},
   ]);
 
   return(
     <App>
       <div className="friend-list">
-       ใส่โค้ดที่นี่
+        {
+          friends.map(
+            item => {
+                return (<div className='friend-list-item'>
+                    <Avatar name={item.user}/>
+                    <div className="text">{item.displayName}</div>
+                    </div>
+                    )
+            }
+        )
+        }
       </div>
     </App>
   )
